a5ffc07bc752bd09b5dbd5964a062a32c99d320f
K2 <email@k2.com>
2019-07-01 23:45:35 +0300

    Finished arp scan

--- netscanner.py
+++ netscanner.py
@@ -4,8 +4,24 @@ def ArpRequest(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
-    print(answered.summary())
+    answered = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    
+    target_list = []
+
+   
+    for item in answered:
+        target_item = {
+            "ip": item[1].psrc,
+            "mac":item[1].hwsrc
+        }
+        target_list.append(target_item)
+    return target_list
+
+def ShowArpScan(target_list):
+     print("IP\t\t\tMAC Address\n--------------------------------")
+     for target in target_list:
+         print(target["ip"] + "\t\t"  + target["mac"])
 
 
-ArpRequest("192.168.1.1/24")
+arp_scan = ArpRequest("192.168.1.0/24")
+ShowArpScan(arp_scan)
