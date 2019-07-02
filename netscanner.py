1e6c97d5c9d9607db09b5fa241805fd22570eda6
K2 <email@k2.com>
2019-07-02 23:52:22 +0300

    Fixed arguments module

--- netscanner.py
+++ netscanner.py
@@ -1,10 +1,13 @@
 import argparse
 from modules import arp
 
+
+
 def get_arguments():
     parser = argparse.ArgumentParser(description= "Net Scanner usage ")
-    parser.add_argument("-t", "--target", nargs=2, dest="target",  help="Target IP (192.168.1.1) / IP range (192.168.1.0/24) ")
-    parser.add_argument("-a", "--arp",   help="Scan network using arp")
+    #parser.add_argument("-a", "--arp", nargs="+",  help="Scan network using arp", "-t", "--target", dest="target", help="Scan network using arp/ Target IP (192.168.1.1) / IP range (192.168.1.0/24) ")
+    parser.add_argument("-p", "--proto ",  default="arp", dest="proto",  help="Scan network using network protocols")
+    parser.add_argument("-t", "--target", dest="target", help="Target IP (192.168.1.1) / IP range (192.168.1.0/24) ")
     options = parser.parse_args()
     return options
 
@@ -12,6 +15,9 @@ def get_arguments():
 
 options = get_arguments()
 print(options)
+if options.proto == "arp":
+    arp_scan = arp.ArpScan(options.target)
+    arp.ShowArpScan(arp_scan)
 
-arp_scan = arp.ArpScan(options.target)
-arp.ShowArpScan(arp_scan)
+    
+
